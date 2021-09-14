66b9cdd6a9a8e9361afdcb50404cdb02c396c28a
levitacija <dudukin.matvey@mail.ru>
2021-09-14 19:59:48 +0300

    Добавьте файлы проекта.

---
+++ main.c
@@ -0,0 +1,12 @@
+
+#include <stdio.h>        
+int main(void)                  
+{                               
+    printf("Hello world! \n");
+    int first, second, third;
+    scanf("%d", &first);
+    scanf("%d", &second);
+    scanf("%d", &third);
+    printf("%d", first - second);
+    return 0;                   
+}
